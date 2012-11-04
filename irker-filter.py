9ffe36effbcde82491ecda3ff4a44f8b86c6c571
Alexander van Gessel (AI0867) <ai0867@gmail.com>
2012-11-04 03:48:07 +0100

    Move all the filtering into a class

--- irker-filter.py
+++ irker-filter.py
@@ -32,62 +32,61 @@ FILTER = "filter.json"
 if __name__ == "__main__":
     packet_queue = Queue.Queue()
 
-    def sane(packet):
-        if len(packet) != 2:
-            return False
-        if "to" not in packet:
-            return False
-        if "privmsg" not in packet:
-            return False
-        targets = packet["to"]
-        if isinstance(targets, basestring):
-            return True
-        if not isinstance(targets, list):
-            return False
-        for target in targets:
-            if not isinstance(target, basestring):
+    class Filter(object):
+        def __init__(self, filename):
+            self.filters = json.load(open(filename, "r"))
+        def sane(self, packet):
+            if len(packet) != 2:
                 return False
-        return True
-
-    def match(filt, packet, peer):
-        for k,v in filt.items():
-            if k == "host":
-                try:
-                    hosttuple = socket.gethostbyname_ex(v)
-                    if peer[0] not in hosttuple[2]:
-                        return False
-                except:
-                    return False
-            elif k == "ip":
-                # TODO: CIDR
-                if v != peer[0]:
-                    return False
-            elif k == "to":
-                targets = packet["to"]
-                if not isinstance(targets, list):
-                    targets = [targets]
-                for target in targets:
-                    if not re.match(v, target):
-                        return False
-            elif k == "privmsg":
-                if not re.match(v, packet["privmsg"]):
-                    return False
-        return True
-
-    def do_filter(packet, peer):
-        debug("Got message {0} from peer {1}:{2}... ".format(packet,peer[0],peer[1]))
-        filters = json.load(open(FILTER,"r"))
-        packet = json.loads(packet)
-        if not sane(packet):
-            debug("not sane\n")
-            return False
-        for f in filters:
-            if match(f, packet, peer):
-                debug("OK\n")
+            if "to" not in packet:
+                return False
+            if "privmsg" not in packet:
+                return False
+            targets = packet["to"]
+            if isinstance(targets, basestring):
                 return True
-        debug("rejected\n")
-        return False
-
+            if not isinstance(targets, list):
+                return False
+            for target in targets:
+                if not isinstance(target, basestring):
+                    return False
+            return True
+        def match(self, packet, peer):
+            debug("Got message {0} from peer {1}:{2}... ".format(packet,peer[0],peer[1]))
+            packet = json.loads(packet)
+            if not self.sane(packet):
+                debug("not sane\n")
+                return False
+            for f in self.filters:
+                if self.filter_match(f, packet, peer):
+                    debug("OK\n")
+                    return True
+            debug("rejected\n")
+            return False
+        def filter_match(self, filt, packet, peer):
+            for k,v in filt.items():
+                if k == "host":
+                    try:
+                        hosttuple = socket.gethostbyname_ex(v)
+                        if peer[0] not in hosttuple[2]:
+                            return False
+                    except:
+                        return False
+                elif k == "ip":
+                    # TODO: CIDR
+                    if v != peer[0]:
+                        return False
+                elif k == "to":
+                    targets = packet["to"]
+                    if not isinstance(targets, list):
+                        targets = [targets]
+                    for target in targets:
+                        if not re.match(v, target):
+                            return False
+                elif k == "privmsg":
+                    if not re.match(v, packet["privmsg"]):
+                        return False
+            return True
     class UDPHandler(SocketServer.BaseRequestHandler):
         def handle(self):
             data = self.request[0].strip()
@@ -99,16 +98,17 @@ if __name__ == "__main__":
                 if not line:
                     break
                 packet_queue.put((line, self.request.getpeername()))
-    class IrkerConn:
+    class IrkerConn(object):
         def __init__(self, target):
             self.target = target
             self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            self.filterer = Filter(FILTER)
         def send(self, packet):
             self.sock.sendto(packet, self.target)
         def serve_forever(self):
             while True:
                 packet, peer = packet_queue.get()
-                if do_filter(packet, peer):
+                if self.filterer.match(packet, peer):
                     self.send(packet)
 
     tcpserver = SocketServer.TCPServer((HOST, PORT), TCPHandler)
