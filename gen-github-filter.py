8862f31fa193a165028fa9cf9ed0fb6f213bef56
Alexander van Gessel <ai0867@gmail.com>
2013-04-20 00:07:52 +0200

    Don't overwrite existing filters

--- gen-github-filter.py
+++ gen-github-filter.py
@@ -5,4 +5,4 @@ import urllib2
 
 response = json.load(urllib2.urlopen("https://api.github.com/meta"))
 filt = [ {"ip":response["hooks"], "to":"irc://chat.freenode.net/.*"} ]
-json.dump(filt, open("filter.json", "w"), indent=True)
+json.dump(filt, open("filter-github.json.sample", "w"), indent=True, separators=(',', ':'))
